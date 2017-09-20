48142d967078fde03b236505e5b46f24aae825ed
Derek T Burk <burkx031@umn.edu>
2017-09-20 13:25:19 -0500

    Load and tabulate in one step

--- 1_load_data_and_tabulate.R
+++ 1_load_data_and_tabulate.R
@@ -1,6 +1,70 @@
 library(ripums)
+library(maps)
 library(tidyverse)
 
-usa_ddi <- read_ipums_ddi("original_data/usa_00008.xml")
 
-d <- read_ipums_micro(usa_ddi, n_max = 30000)
+# Parameters for testing --------------------------------------------------
+
+N_MAX <- 30000
+
+
+# Directories -------------------------------------------------------------
+DATA_DIR <- "original_data"
+
+
+# Files -------------------------------------------------------------------
+DDI_FILE <- "usa_00008.xml"
+OUT_FILE_PATTERN <- "1_homeownership_by_group_%s.csv"
+
+
+# Load data ---------------------------------------------------------------
+
+usa_ddi <- read_ipums_ddi(
+    file.path(
+        DATA_DIR,
+        DDI_FILE
+    )
+)
+
+d <- read_ipums_micro(usa_ddi, n_max = N_MAX)
+
+
+# Make reports ------------------------------------------------------------
+
+make_report <- function(data) {
+    data %>%
+        group_by(YEAR, RACE, SEX, URBAN, FARM, STATEFIP) %>%
+        summarise(
+            n_own = sum(PERWT * (OWNERSHP == "Owned or being bought (loan)"), na.rm=TRUE), 
+            n_free_and_clear = sum(PERWT * FREE_AND_CLEAR, na.rm=TRUE), 
+            n_group = sum(PERWT)
+        ) %>%
+        mutate(
+            pct_own = 100 * n_own / n_group, 
+            pct_free_and_clear = 100 * n_free_and_clear / n_group
+        )
+}
+
+d <- d %>%
+    mutate_if(is.labelled, as_factor) %>%
+    mutate(HH_HEAD = RELATE == "Head/Householder") %>%
+    mutate(FREE_AND_CLEAR = MORTGAGE == "No, owned free and clear")
+    
+d %>%
+    make_report() %>%
+    write_csv(
+        sprintf(
+            OUT_FILE_PATTERN, 
+            "all_individuals"
+        )
+    )
+    
+d %>%
+    filter(HH_HEAD) %>%
+    make_report() %>%
+    write_csv(
+        sprintf(
+            OUT_FILE_PATTERN, 
+            "hh_heads_only"
+        )
+    )
